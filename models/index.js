7a5c5279c71151e92aa169419ae933419cb088ba
Brian Samuels <samuels.brian9@gmail.com>
2022-05-03 21:23:08 -0400

    refactoring databases

--- models/index.js
+++ models/index.js
@@ -3,15 +3,27 @@ const Product = require('./Product');
 const Category = require('./Category');
 const Tag = require('./Tag');
 const ProductTag = require('./ProductTag');
+const { belongsTo } = require('./Product');
 
 // Products belongsTo Category
-
+Product.belongsTo(Category, {
+  foreignKey: 'category_id',
+  onDelete: 'CASCADE'
+});
 // Categories have many Products
-
+Category.hasMany(Product, {
+  key: 'id'
+});
 // Products belongToMany Tags (through ProductTag)
-
+Product.belongsToMany(Tag, {
+  through: ProductTag,
+  foreignKey: 'product_id'
+});
 // Tags belongToMany Products (through ProductTag)
-
+Tag.belongsToMany(Product, {
+  through: ProductTag,
+  foreignKey: 'tag_id'
+});
 module.exports = {
   Product,
   Category,
